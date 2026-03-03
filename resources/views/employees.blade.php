df7d7a0dc16371a8bfec02f23e3df70122c670c4
othman rasfi el qabiti <oraselq824@gmail.com>
2026-03-03 15:38:43 +0100

    commit de prueba

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Componentes de Empleados</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script> 
 </head>
 
 <body class="p-10 bg-gray-100">
